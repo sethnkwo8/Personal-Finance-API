13e8c97f2a1d509e4f3986f9b82586399a978401
Seth Nkwo <sethnkwocool@gmail.com>
2026-04-28 14:46:34 +0100

    Create login user service

--- finance-api-ts/src/services/auth.service.ts
+++ finance-api-ts/src/services/auth.service.ts
@@ -31,7 +31,37 @@ export async function signUpUser(
     // Return user details
     return {
         id: user._id,
-        name: user.name,
+        name: user.email,
         email: user.email
     };
+}
+
+// Login user service
+export async function loginUser(
+    email: string,
+    password: string
+) {
+    const user = await User.findOne({email})
+
+    if (!user) {
+        const err = new Error("User not found");
+        (err as any).statusCode = 404;
+        throw err
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        const err = new Error("Incorrect password");
+        (err as any).statusCode = 401;
+        throw err
+    }
+
+    const token = jwt.sign(
+        {userId: user._id},
+        process.env.JWT_SECRET as string,
+        {expiresIn: "1h"}
+    )
+
+    return token
 }
