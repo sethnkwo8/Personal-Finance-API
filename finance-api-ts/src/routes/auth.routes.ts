1537fc42e4b20b1e1a3ef7998ba58662100007b3
Seth Nkwo <sethnkwocool@gmail.com>
2026-04-28 15:39:50 +0100

    Added validation to routes

--- finance-api-ts/src/routes/auth.routes.ts
+++ finance-api-ts/src/routes/auth.routes.ts
@@ -1,13 +1,15 @@
 // Auth routes
 import express from "express"
 import { signUp, login } from "../controllers/auth.controller.js"
+import { validate } from "../middleware/validate.js"
+import { signUpSchema, loginSchema } from "../validators/auth.validator.js"
 
 const router = express.Router()
 
 // Signup route
-router.post('/signup', signUp)
+router.post('/signup', validate(signUpSchema), signUp)
 
 // Login route
-router.post('/login', login)
+router.post('/login', validate(loginSchema), login)
 
 export default router
