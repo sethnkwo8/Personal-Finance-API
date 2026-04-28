5e0912e0c97e8df42a499ac02f483962c910edc5
Seth Nkwo <sethnkwocool@gmail.com>
2026-04-28 15:39:30 +0100

    Created middleware for validation

---
+++ finance-api-ts/src/middleware/validate.ts
@@ -0,0 +1,19 @@
+// Validate middleware
+import { Request, Response, NextFunction } from "express";
+import { ZodSchema } from "zod";
+
+export function validate(schema: ZodSchema) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const result = schema.safeParse(req.body);
+
+        if (!result.success) {
+            return res.status(400).json({
+                errors: result.error.issues.map(err => err.message)
+            })
+        }
+
+        req.body = result.data;
+
+        next()
+    }
+}
