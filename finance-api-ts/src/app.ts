fa29de83af375cf4b6d13dedadee7c93eee4eded
Seth Nkwo <sethnkwocool@gmail.com>
2026-04-28 14:58:44 +0100

    Connected auth routes to app

--- finance-api-ts/src/app.ts
+++ finance-api-ts/src/app.ts
@@ -1,12 +1,16 @@
 // app setup
 import express from "express";
 import cors from "cors";
+import authRoutes from "./routes/auth.routes.js"
 
 const app = express()
 
 app.use(cors());
 
 app.use(express.json());
+
+// Auth routes
+app.use("/auth", authRoutes)
 
 export default app
 
